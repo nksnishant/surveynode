d330ec06bbc4bde95b7508830c445e7f7d43b547
killerpro07 <killerpro07@gmail.com>
2020-04-04 22:31:26 +0530

    Updated env variable

--- config.js
+++ config.js
@@ -4,33 +4,33 @@ const convict = require("convict");
 
 const config = convict({
   env: {
-    format: ["prod", "dev", "test"],
+    format: ["production", "dev", "test"],
     default: "dev",
     arg: "nodeEnv",
-    env: "NODE_ENV"
+    env: "NODE_ENV",
   },
   port: {
     doc: "The port to bind.",
     format: "port",
     default: 3000,
     env: "PORT",
-    arg: "port"
+    arg: "port",
   },
   mailconn: {
     host: process.env.MAIL_HOST || "smtp.mailtrap.io",
     port: process.env.MAIL_PORT || 2525,
     auth: {
       user: process.env.AUTH_USER || "9c8420a170a4df",
-      pass: process.env.AUTH_PASSWORD || "94bd3df888e72e"
-    }
+      pass: process.env.AUTH_PASSWORD || "94bd3df888e72e",
+    },
   },
   db: {
     database: process.env.DATABASE_NAME || "ceo",
     username: process.env.DB_USER_NAME || "user",
     password: process.env.DB_PASSWORD || "password",
     dialect: process.env.DIALECT || "mysql",
-    dbhost: process.env.DBHOST || "localhost"
-  }
+    dbhost: process.env.DBHOST || "localhost",
+  },
 });
 
 // const env = config.get("env");
